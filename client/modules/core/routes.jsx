a9d97866bbe56eac65490b5236ea8f8f302300d1
hungqt <hqthieu@gmail.com>
2016-10-27 19:02:52 +0200

    lmao

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -3,15 +3,27 @@ import {mount} from 'react-mounter';
 
 import Layout from './components/MainLayout.jsx';
 import ItemList from '../items/components/ItemList.jsx';
+import EditItem from '../items/components/EditItem.jsx';
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(Layout);
 
+  //Router for homepage "/"
   FlowRouter.route('/', {
     name: 'items.list',
     action() {
       mount(MainLayoutCtx, {
         content: () => (<ItemList />)
+      });
+    }
+  });
+
+  //Router for editpage "/edit"
+  FlowRouter.route('/edit', {
+    name: 'items.edit',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<EditItem />)
       });
     }
   });
