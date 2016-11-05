965801c28b3359ba376f4a423cc50649c8a799b0
hungqt <hqthieu@gmail.com>
2016-11-05 20:39:38 +0100

    stable

--- client/modules/items/components/MapView.jsx
+++ client/modules/items/components/MapView.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {Row, Col, Panel} from 'react-bootstrap';
+import Map, {GoogleApiWrapper} from 'google-maps-react';
 
-const MapView = ({content}) => (
-	<div>
-	  <Row>
-	  	<Col xs={12}>
-	  		<h2> Map To Be Implemented</h2>
-	  	</Col>
-	  </Row>
-	</div>
-);
+export class MapView extends React.Component {
+	render(){
+		return(
+			<div>
+			  <Row>
+			  	<Col xs={12}>
+			  		<h2> Map To Be Implemented</h2>
+			  	</Col>
+			  </Row>
+			</div>
+		)
+	}
+}
 
 export default MapView;
