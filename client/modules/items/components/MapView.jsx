2b4b1587c8a8865a8fd1fb2bce4fbdcb15498c36
hungqt <hqthieu@gmail.com>
2016-11-05 21:11:25 +0100

    map is up baby, still lookin ruff tho

--- client/modules/items/components/MapView.jsx
+++ client/modules/items/components/MapView.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Row, Col, Panel} from 'react-bootstrap';
 import Map, {GoogleApiWrapper} from 'google-maps-react';
 
+
 export class MapView extends React.Component {
 	render(){
 		return(
@@ -11,9 +12,15 @@ export class MapView extends React.Component {
 			  		<h2> Map To Be Implemented</h2>
 			  	</Col>
 			  </Row>
+				<Map google={this.props.google} zoom={14} >
+
+				</Map>
 			</div>
 		)
 	}
 }
 
-export default MapView;
+export default GoogleApiWrapper({
+	//apiKey should load from a global variable, but i didn't manage to make it work.
+	apiKey: 'AIzaSyCTDaKwjj1RxHX-hi8UjtqJbyHCerKPSRg'
+})(MapView);
